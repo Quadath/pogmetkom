cb4dfcabfaedf53d18cb9326bd1c5e1e7f23c0b9
Roman <quadath@gmail.com>
2024-08-13 13:10:25 +0300

    menus 1

--- bot.js
+++ bot.js
@@ -1,25 +1,42 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
 
 bot.launch()
 
-bot.command("inline", (ctx) => {
-    ctx.reply("Hi there!", {
-        reply_markup: {
-            inline_keyboard: [
-                /* Inline buttons. 2 side-by-side */
-                [ { text: "Button 1", callback_data: "btn-1" }, { text: "Button 2", callback_data: "btn-2" } ],
+bot.start((ctx) => 
+    {
+        ctx.state.one = 'onee'
+        console.log(ctx.state)
+    ctx.reply('this is text', Markup
+        .keyboard([
+          ['Памятка нового сотрудника'], 
+          ['Инструктаж по Охране Труда'],
+          ['Регламенты', 'Кайдзен'], 
+          ['В начало', 'Назад',]
+        ])
+        .oneTime()
+        .resize()
+      )
+    }
+)
 
-                /* One button */
-                [ { text: "Next", callback_data: "next" } ],
-                
-                /* Also, we can have URL buttons. */
-                [ { text: "Open in browser", url: "telegraf.js.org" } ]
-            ]
-        }
-    });
-});
+
+bot.hears('Памятка нового сотрудника', (ctx) => {
+    console.log(ctx.state)
+    ctx.reply('this is text', Markup
+        .keyboard([
+          ['Контакты', 'О компании'], 
+          ['Оплата труда', 'Наши правила'],
+          ['В начало', 'Назад',]
+        ])
+        .oneTime()
+        .resize()
+      )
+})
+
+
+
+console.log('successfully started')
