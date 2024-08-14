9bc6ac38edce22f61c580ba2a777ebb9f39003a0
Roman <quadath@gmail.com>
2024-08-14 09:29:07 +0300

    setting up wizard scenes 1

--- bot.js
+++ bot.js
@@ -1,41 +1,55 @@
-const { Telegraf, Markup } = require('telegraf')
+const { Telegraf, Markup, Stage, Scenes, session } = require('telegraf')
 
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.TOKEN)
-
 bot.launch()
 
-bot.start((ctx) => 
-    {
-        ctx.state.one = 'onee'
-        console.log(ctx.state)
-    ctx.reply('this is text', Markup
-        .keyboard([
-          ['Памятка нового сотрудника'], 
-          ['Инструктаж по Охране Труда'],
-          ['Регламенты', 'Кайдзен'], 
-          ['В начало', 'Назад',]
-        ])
-        .oneTime()
-        .resize()
-      )
+
+const MainScene = new Scenes.WizardScene('MAIN_SCENE',
+  (ctx) => {
+    ctx.reply('Выберите подходящую опцию ниже.', Markup
+      .keyboard([
+        ['Памятка нового сотрудника'], 
+        ['Инструктаж по Охране Труда'],
+        ['Регламенты', 'Кайдзен'], 
+        ['В начало', 'Назад',]
+      ])
+      .oneTime()
+      .resize()
+    )
+    return ctx.wizard.next();
+  }, 
+  (ctx) => {
+    switch(ctx.message.text) {
+      case 'Памятка нового сотрудника': ctx.scene.enter('NEW_STAFF_SCENE')
     }
+  }
+)
+const NewStaffScene = new Scenes.WizardScene("NEW_STAFF_SCENE", 
+  (ctx) => {
+    ctx.reply('Памятка нового сотрудника', Markup
+      .keyboard([
+        ['Контакты', 'О компании'], 
+        ['Оплата труда', 'Наши правила'],
+        ['В начало', 'Назад',]
+      ])
+      .oneTime()
+      .resize()
+    )
+    return ctx.wizard.next();
+  }
 )
 
+const stage = new Scenes.Stage([MainScene, NewStaffScene])
 
-bot.hears('Памятка нового сотрудника', (ctx) => {
-    console.log(ctx.state)
-    ctx.reply('this is text', Markup
-        .keyboard([
-          ['Контакты', 'О компании'], 
-          ['Оплата труда', 'Наши правила'],
-          ['В начало', 'Назад',]
-        ])
-        .oneTime()
-        .resize()
-      )
-})
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.start((ctx) => ctx.scene.enter('MAIN_SCENE'))
+
+
+
 
 
 
