2a77707b2061cec2e34ba02f328a4452cd65bbca
Roman <quadath@gmail.com>
2024-08-28 08:57:46 +0300

    1.2.3 bug fix

--- scenes/1.2 Labor Protection Briefing/1.2.3 At The Workplace/index.js
+++ scenes/1.2 Labor Protection Briefing/1.2.3 At The Workplace/index.js
@@ -25,8 +25,8 @@ const BriefingAtTheWorkplaceScene = new Scenes.WizardScene("BRIEFING_AT_THE_WORK
           ctx.scene.enter('EMPTY_SCENE')
         } break;
         case 'Назад': ctx.scene.enter('LABOR_PROTECTION_SCENE'); break;
-        case 'В начало': ctx.scene.enter('SCENE'); break;
-        default: ctx.scene.enter('SCENE')
+        case 'В начало': ctx.scene.enter('MAIN_SCENE'); break;
+        default: ctx.scene.enter('MAIN_SCENE')
       }
     }
   )
